71bca3c37dccbef21de7330e2021daec57db234f
Omar Andrés Solarte Ospina <omarsolarte@jeduca.com>
2022-12-28 15:54:18 -0500

    add formHR

---
+++ src/webparts/formularioHr/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface IFormularioHrWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'FormularioHrWebPartStrings' {
+  const strings: IFormularioHrWebPartStrings;
+  export = strings;
+}
